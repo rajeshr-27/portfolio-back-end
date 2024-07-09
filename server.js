78484e16cfbeebc23e4f3be4a6f931a9cbaf22df
rajesh-27 <rajeshkphp27@gmail.com>
2024-07-09 08:31:13 +0530

    update reactjs-bulid

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express= require('express');
+const express= require('express'); 
 require('dotenv').config()
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/dbConnection');
 connectDB();
 const app = express();
+app.use(express.static(path.join(__dirname + "/public")))
 app.use(cors());
 app.use('/api', express.static('uploads'));
 app.use('/api/user', require('./route/userRoute'));
 app.use('/api/region', require('./route/regionRoute'));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'/public','index.html'))
+})
 // app.use((err,req,res,next)=> {
 //     console.log(err);
 // }) 
